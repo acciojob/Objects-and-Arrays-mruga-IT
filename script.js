c45f8e8bab6d56c52f05d555625de9ee3f799881
mruga-IT <124659354+mruga-IT@users.noreply.github.com>
2023-03-16 14:53:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const players = ['John', 'Bob', 'Alice', 'Poppy'];
+// const players = ['John', 'Bob', 'Alice', 'Poppy'];
 
-const person = {
-      name: 'John Doe',
-      age: 80
-    };
+// const person = {
+//       name: 'John Doe',
+//       age: 80
+//     };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+const players = ["John", "Jane", "Bob", "Alice"];
+const person = {
+  name: "Jack",
+  age: 30,
+  nationality: "USA"
+};
 
+const team = players; // storing reference to players array
+const team1 = [...players]; // creating a copy of players array using spread operator
+const cap1 = {...person};
 
 export {players,person,team,team1,cap1}
