754268b0bc575947f83327c51cc99ca42e8645b7
mruga-IT <124659354+mruga-IT@users.noreply.github.com>
2023-04-21 12:46:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,18 @@
 //       name: 'John Doe',
 //       age: 80
 //     };
+const players = ['John', 'Bob', 'Alice', 'Poppy'];
 
+const person = {
+  name: 'John Doe',
+  age: 80
+};
+
+const team = players;
+const team1 = [...players];
+const cap1 = {...person};
+
+export {players, person, team, team1, cap1};
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 const players = ["John", "Jane", "Bob", "Alice"];
 const person = {
